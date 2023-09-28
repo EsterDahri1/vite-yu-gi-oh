c883eddfcac23e156f80352cc71a303e49971132
EsterDahri <esterdahri@gmail.com>
2023-09-28 05:59:45 +0200

    fix cards style and appear

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -16,12 +16,12 @@ export default {
 <template>
     <div class="col">
         <div class="card border-0 rounded-0">
-            <img src="../assets/img/Yu-Gi-Oh!.png" alt="yuGiOh card" class="card-img-top">
-            <div class="card-body">
-                <div class="name">
-                    {{ card.name }}
+            <img :src="card.card_images[0].image_url" alt="yuGiOh card" class="card-img-top">
+            <div class="card-body d-flex flex-column align-items-center text-center">
+                <div class="name text-white text-uppercase pb-2">
+                    <strong> {{ card.name }} </strong>
                 </div>
-                <div class="archetype">
+                <div class="archetype text-black text-capitalize">
                     {{ card.archetype }}
                 </div>
             </div>
@@ -31,4 +31,18 @@ export default {
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
+
+.col {
+    width: calc(100% / 10 * 2);
+
+    .card {
+        background-color: $ygoWarning;
+        width: 190px;
+        height: 100%;
+
+        img {
+            width: 100%;
+        }
+    }
+}
 </style>
